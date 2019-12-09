c1f018c931c7bfc8ee696cd4ea4478f2544c642a
Muhammad Yasir Nawaz <Muhammad.Nawaz@kpmg.co.uk>
2019-12-09 10:19:26 +0000

    removed comment

--- src/components/App.js
+++ src/components/App.js
@@ -22,31 +22,3 @@ const App = () => {
 };
 
 export default App;
-
-// import React, { useState } from 'react';
-// import TextBoxControl from './TextBoxControl';
-
-// function App(props) {
-//   const [counter, setCounter] = useState(0);
-
-//   const updateCounter = val => {
-//     setCounter(parseInt(val));
-//   };
-
-//   return (
-//     <div>
-//       <TextBoxControl
-//         id='testid'
-//         name='testid'
-//         placeholder='test textbox'
-//         updateCounter={updateCounter}
-//       ></TextBoxControl>
-//       <br />
-//       {props.message} {counter}
-//       <br /> <br />{' '}
-//       <button onClick={() => setCounter(counter + 1)}>Increase Number</button>
-//     </div>
-//   );
-// }
-
-// export default App;
